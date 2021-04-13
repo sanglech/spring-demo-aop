3419579735796f7649479fdbf9128e35bd27e588
Christian <christian@sanglech.com>
2021-04-13 20:01:00 +0900

    Changed type for addPerson for AOP point cut expression test

--- src/com/christian/aopdemo/dao/MembershipDAO.java
+++ src/com/christian/aopdemo/dao/MembershipDAO.java
@@ -7,7 +7,8 @@ public class MembershipDAO {
 	public void addAccount() {
 		System.out.println(getClass()+ ": doing DB work adding an account");	
 		}
-	public void addPerson() {
-		System.out.println(getClass()+ ": Adding person");	
+	public boolean addPerson() {
+		System.out.println(getClass()+ ": Adding person");
+		return true;
 		}
 }
