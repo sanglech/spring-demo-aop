b86dbaa80c536b3302acc78549874c896a2fd724
Christian <christian@sanglech.com>
2021-04-13 19:32:35 +0900

    AOP point cut expression matching on many methods
    -Added MembershipDAO

--- src/com/christian/aopdemo/MainDemoApp.java
+++ src/com/christian/aopdemo/MainDemoApp.java
@@ -3,6 +3,7 @@ package com.christian.aopdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.christian.aopdemo.dao.AccountDAO;
+import com.christian.aopdemo.dao.MembershipDAO;
 
 public class MainDemoApp {
 	public static void main(String[]args) {
@@ -13,13 +14,22 @@ public class MainDemoApp {
 		
 		//get bean from spring container
 		AccountDAO theAccountDAO= context.getBean("accountDAO",AccountDAO.class);
+		MembershipDAO theMembershipDAO=  context.getBean("membershipDAO",MembershipDAO.class);
+		
+		
+		//point cut expression matches on ALL add account methods
+		
 		
 		//call the buisness method
-		
-		theAccountDAO.addAccount();
-		
 		System.out.println("Adding another account");
 		theAccountDAO.addAccount();
+
+		
+		System.out.println("Adding membership DAO");
+		theMembershipDAO.addAccount();
+		
+		
+		
 		
 		//close the context
 		context.close();
